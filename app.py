95f96defa6c93395aa40636b820bd9558949f2e6
costero-e <orildsaga@gmail.com>
2021-05-13 10:22:43 +0200

    nova3

--- app.py
+++ app.py
@@ -29,21 +29,14 @@ from tensorflow.keras.models import Sequential
 import urllib.request
 import sys, getopt
 import random
-# from fileinput import filename
-# import urllib
 
 filename = "unknown"
-# folderPath = '/Users/barnatasa/Desktop/Nou3/'
 folderPath = os.getcwd() + '/'
 print("folderPath:", folderPath)
 
 
 
 def image_to_array(image_path):
-    """
-    Input: Image path
-    Output: Flatten array of 3x128x128 image pixels in range[0,255]
-    """
     im = Image.open(image_path)
     return np.array(im, dtype=float)
 
@@ -56,45 +49,31 @@ def label_decoder(key):
     return label_mapping[key]
 
 def run_model(data):
-    """
-    Load Keras model (.yaml) and weights (.h5), and predict image label
-    Input: Image pixels array
-    Output: Predicted label
-    """
-    
-    # Load Keras Model
+
     loaded_model = load_model('gear_classifier_2.model')
 
-    # Predict results array (array of 12 elements, one 1.0 and 11 are 0.0's)
     result_array = loaded_model.predict(data).tolist()[0]
     
-    # Get the position of the element 1.0 within the array
     index = result_array.index(max(result_array)) + 1
     
-    # Decode results
     predicted_label = label_decoder(str(index))
     
     return predicted_label
 
    
 def resize(image_name, dirPath):
-    """
-    Pick a basic color (Black) and pad the images that do not have a 1:1 aspect ratio.
-    Reshape without stretching to a 128x128 pixel array shape
-    """
     
     image_path = folderPath + image_name
     im = Image.open(image_path)
         
     desired_size = 128
-    old_size = im.size  # old_size[0] is in (width, height) format
+    old_size = im.size 
 
     ratio = float(desired_size)/max(old_size)
     new_size = tuple([int(x*ratio) for x in old_size])
 
     im = im.resize(new_size, Image.ANTIALIAS)
     
-    # create a new image and paste the resized on it
     new_im = Image.new("RGB", (desired_size, desired_size))
     new_im.paste(im, ((desired_size-new_size[0])//2,
                         (desired_size-new_size[1])//2))
@@ -110,23 +89,14 @@ def resize(image_name, dirPath):
     
     return new_filename
 
-def equalize_image(image_name, dirPath): #"imagename_resized.JPEG"
-    """
-    Ensure for each image that the pixel range is [0,255] (constrast stretching)
-    by applying the equalise method (normalise works also)
-    """
+def equalize_image(image_name, dirPath): 
     
     image_path = folderPath+ image_name
     im = Image.open(image_path)
     
-    # Plotting histogram for resized image
-    #im_array = np.array(im)
-    #plt.hist(im_array.flatten(), bins=50, range=(0.0, 300))
-    
-    # Equalize image
+
     im_out = ImageOps.equalize(im)
     
-    # Save equalized image
     filename, file_extension = os.path.splitext(image_name) 
     new_filename = filename + "_equalized.jpeg"
     im_out.save(folderPath + new_filename, "JPEG")
@@ -136,9 +106,7 @@ def equalize_image(image_name, dirPath): #"imagename_resized.JPEG"
 
 def return_prediction():
     
-    # For larger data features, you should probably write a for loop
-    # That builds out this array for you
-       
+     
     list_of_files = glob.glob('static/*') 
     
     latest_file = max(list_of_files, key=os.path.getctime)
@@ -153,19 +121,20 @@ def return_prediction():
     numpy_data = numpy_data.astype('float32')
     numpy_data = normalize(numpy_data)
     result_label = run_model(numpy_data)
-    print('Predicted : ' + result_label + '\n')
+    model = load_model('gear_classifier_2.model')
+    predictions = model.predict(numpy_data)
+    score = tf.nn.softmax(predictions)
+    message = "Esta imagen pertenece a un tumor del tipo {} con una precisión de {:.2f} sobre 100."
+    print(message.format(result_label, round(100 * np.max(score))))
     
-    return 'Predicted : ' + result_label + '\n'
+    return message.format(result_label, round(100 * np.max(score)))
   
      
 
 app = Flask(__name__)
-# Configure a secret SECRET_KEY
-# We will later learn much better ways to do this!!
+
 app.config['SECRET_KEY'] = 'someRandomKey'
 
-# Now create a WTForm Class
-# Lots of fields available:
 # http://wtforms.readthedocs.io/en/stable/fields.html
 class UploadForm(FlaskForm):
     file = FileField()
@@ -178,6 +147,9 @@ def index():
     form = UploadForm()
     
     if form.validate_on_submit():
+        files = glob.glob('static/*') 
+        for f in files:
+            os.remove(f)
         filename = secure_filename(form.file.data.filename)
         print("filename1:")
         print(filename)
